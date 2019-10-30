2712977259bc588b7097b5ebc67982361bc0b94d
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-10-30 12:07:59 +0000

    Add Items component

---
+++ src/components/Items.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Items extends Component {
+  render() {
+    return (
+      <div>
+        <h1>All items</h1>
+      </div>
+    )
+  }
+}
