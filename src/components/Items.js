2cf6edd8e9c2c26b7beeb702cd34b059c9f42ec2
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-10-30 12:46:51 +0000

    Get items(users for now) from backend API

--- src/components/Items.js
+++ src/components/Items.js
@@ -1,11 +1,31 @@
 import React, { Component } from 'react'
+import axios from 'axios'
 
 export default class Items extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { data: [] }
+  }
+
+    componentDidMount() {
+      const url = 'https://pythonflaskbooks.herokuapp.com/getallusers'
+
+      axios.get(url)
+        .then(response => {
+          console.log(response)
+          this.setState({ data: response.data })
+        })
+    }
+
   render() {
     return (
-      <div>
-        <h1>All items</h1>
-      </div>
+      <p>
+        {this.state.data.map(function(user, i){
+          return (
+            <li key={i}>Username: {user.username}<br></br>Email: {user.email}</li>
+            )
+        })}
+      </p>
     )
   }
 }
