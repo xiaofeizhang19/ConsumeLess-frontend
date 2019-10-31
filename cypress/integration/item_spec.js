1494b94ed424def5bb5941988fd9e60c6eabca21
Neil Campbell <neildcampbell@hotmail.com>
2019-10-31 15:37:04 +0000

    added cypress tests

---
+++ cypress/integration/item_spec.js
@@ -0,0 +1,11 @@
+describe('Items', function(){
+    it('displays random items from API', function (){
+        cy.request('https://pythonflaskbooks.herokuapp.com/getallusers')
+        .should((response)=> {
+            console.log(response)
+            expect(response.status).to.eq(200)
+            expect(response.body[0]).to.have.property('username')
+            expect(response.body[0]).to.have.property('email')
+        })
+    })
+})
