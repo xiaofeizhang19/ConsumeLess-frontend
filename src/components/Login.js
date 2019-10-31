b231f65f96d8360aa63f910e24c878e3c4fb7571
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-10-31 12:19:05 +0000

    Add username password and submit button

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,71 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
 
+class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
 
-export default class Login extends Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome to ConsumeLess!</h1>
-            </div>
-        )
-    }
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange = ({ target }, type) => {
+    this.setState({
+      ...this.state,
+      [type]: target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    console.log(event.target)
+    const data = "hello"
+    const url = "http://localhost:1337/myfile.html"
+    fetch(url, {
+      method: 'POST',
+      body: data,
+    })
+  }
+
+  render() {
+    const { username, password } = this.state;
+    return (
+      <form>
+        <label>Username:</label>
+        <input
+          id="username"
+          value={username}
+          onChange={event => this.handleChange(event, "username")}
+        />
+        <br />
+        <label>Password:</label>
+        <input
+          id="password"
+          type="password"
+          autoComplete="password"
+          value={password}
+          onChange={event => this.handleChange(event, "password")}
+        />
+        <br />
+        <input type="button" value="Log In" onClick={this.handleSubmit} />
+      </form>
+    );
+  }
 }
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
+
+export default Login;
