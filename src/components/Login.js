c33bd73e73ec22ea49f0f5f0a62a536133abb0fc
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-10-31 16:25:44 +0000

    Refactor Login with bootstrap

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,31 +3,69 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
 export default class Login extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange = ({ target }, type) => {
+    this.setState({
+      ...this.state,
+      [type]: target.value
+    });
+  }
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+
+    const data = new FormData(event.target)
+    console.log(data);
+    
+    const url = "http://localhost:1337/myfile.html"
+    fetch(url, {
+      method: 'POST',
+      body: data,
+      mode: 'no-cors',
+    });
+  }
+
   render() {
+    const { username, password } = this.state;
+
     return (
       <div className="container">
         <h1>Welcome to ConsumeLess</h1>
-        <h3>Login here</h3>
-        <Form>
-  <Form.Group controlId="formBasicEmail">
-    <Form.Label>Email address</Form.Label>
-    <Form.Control type="email" placeholder="Enter email" />
-    <Form.Text className="text-muted">
-      We'll never share your email with anyone else.
-    </Form.Text>
-  </Form.Group>
-
-  <Form.Group controlId="formBasicPassword">
-    <Form.Label>Password</Form.Label>
-    <Form.Control type="password" placeholder="Password" />
-  </Form.Group>
-  <Form.Group controlId="formBasicCheckbox">
-    <Form.Check type="checkbox" label="Check me out" />
-  </Form.Group>
-  <Button variant="primary" type="submit">
-    Submit
-  </Button>
-</Form>
+        <br />
+        <Form onSubmit={this.handleSubmit}>
+          <Form.Group>
+            <Form.Label>Username</Form.Label>
+            <Form.Control
+              type="text"
+              id="username"
+              name="username"
+              value={username}
+              onChange={event => this.handleChange(event, "username")}/>
+          </Form.Group>
+          <Form.Group>
+            <Form.Label>Password</Form.Label>
+            <Form.Control
+              type="password"
+              id="password"
+              name="password"
+              autoComplete="password"
+              value={password}
+              onChange={event => this.handleChange(event, "password")}/>
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Log In
+          </Button>
+        </Form>
       </div>
     )
   }
