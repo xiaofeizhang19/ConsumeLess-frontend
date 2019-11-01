769f4e9e2549d606f7872c2914d0562f77f554d2
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-11-01 11:24:37 +0000

    Change AddItem to NewItem

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Register from './components/Register'
 import Login from './components/Login'
 import Items from './components/Items'
-import AddItem from './components/AddItem'
+import NewItem from './components/NewItem'
 
 function App() {
   return (
@@ -15,7 +15,7 @@ function App() {
         <Route exact path='/' component = {Login} />
         <Route exact path='/register' component = {Register} />
         <Route exact path='/items' component = {Items} />
-        <Route exact path='/items/new' component = {AddItem} /> 
+        <Route exact path='/items/new' component = {NewItem} /> 
         </Switch>
       </Router>     
     </div>
