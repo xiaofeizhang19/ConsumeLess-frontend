bf79be00926d23dc8936a68f577924dba8b3caea
Neil Campbell <neildcampbell@hotmail.com>
2019-11-04 18:28:39 +0000

    added button

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react'
 import Card from 'react-bootstrap/Card'
 import getData from "../actions/getData"
 import { URLs } from '../constants/URLs'
+import CardDeck from 'react-bootstrap/CardDeck'
+import Button from 'react-bootstrap/Button'
+
 
 
 export default class Cards extends Component {
@@ -17,25 +20,29 @@ export default class Cards extends Component {
     render() {
       {console.log(this.state.data)}
         return (
+          
           <div>
+            
             {this.state.data.map((item, index) => {
               return (
-                
+             
               <div key={index} className="col-md-6 col-lg-4">
-                <div className="card mb-4">
-                  <Card border="primary" bg ="info">
+                <Card style={{flex:1}} border="primary" bg ="info" className="card mb-4">
+                  <Card.Header>{ item.name }</Card.Header>
                     <Card.Img src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60' alt =''/>
                       <Card.Body>
                         <Card.Title>{ item.name }</Card.Title>
                         <Card.Title>{ item.description }</Card.Title>
-                    </Card.Body>
-                  </Card>
+                        <Button href= {`/items/${item.id}`} variant="dark">View Item</Button>
+                      </Card.Body>
+                </Card>
                 </div>
-              </div> 
+               
               )  
             }
           )}
       </div>
+     
       )
     }
 }
