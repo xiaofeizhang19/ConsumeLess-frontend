93e47a1b327635a1a540ffbedc45c68cf0738fd1
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-11-05 02:09:27 +0000

    Make tests happy

--- cypress/integration/sample_spec.js
+++ cypress/integration/sample_spec.js
@@ -6,9 +6,9 @@ describe('Home Login', function(){
 
     it('it focuses the input', () => {
         cy.visit('http://localhost:3000')
-        cy.get('h1')
-        cy.should(($h1) => {
-            expect($h1).to.contain('Welcome to Consume£e$$')
+        cy.get('h2')
+        cy.should(($h2) => {
+            expect($h2).to.contain('Welcome to Consume£e$$')
         })
       })
 })
