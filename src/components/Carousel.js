abb090476272a697c8c6b59255b74eaedb8430d1
Neil Campbell <neildcampbell@hotmail.com>
2019-11-05 11:59:24 +0000

    added card to carousel

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import InfiniteCarousel from 'react-leaf-carousel';
+import Card from 'react-bootstrap/Card'
 
 export default class Carousel extends Component {
     render() {
@@ -31,11 +32,21 @@ export default class Carousel extends Component {
       slidesToShow={4}
       scrollOnDevice={true}
     >
-      <div>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-        />
+      <div className="container">
+      <Card style={{ width: '18rem' }} border="secondary" >
+  <Card.Body>
+  <Card.Header bg="info">Featured</Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Title>Card Title</Card.Title>
+    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
       </div>
       <div>
         <img
