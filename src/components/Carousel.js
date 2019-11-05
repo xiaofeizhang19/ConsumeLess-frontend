812c1f872bc03c6274d889cb46cb4e0fcf84fba0
Neil Campbell <neildcampbell@hotmail.com>
2019-11-05 12:30:26 +0000

    added cards for each category

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -33,15 +33,36 @@ export default class Carousel extends Component {
       scrollOnDevice={true}
     >
       <div className="container">
-      <Card style={{ width: '18rem' }} border="secondary" >
+      <Card style={{ width: '18rem' }} border="secondary" bg="">
   <Card.Body>
-  <Card.Header bg="info">Featured</Card.Header>
-  <Card.Img variant="top" src="https://images.unsplash.com/photo-1513694203232-719a280e022f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
-    <Card.Title>Card Title</Card.Title>
-    <Card.Subtitle className="mb-2 text-muted">Card Subtitle</Card.Subtitle>
-    <Card.Text>
-      Some quick example text to build on the card title and make up the bulk of
-      the card's content.
+  <Card.Header bg="info"> <Card.Title>Books</Card.Title></Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1550399105-c4db5fb85c18?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Text className="text-center">
+      Find your favourite books here, anything from fiction to non-fiction
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+      </div>
+      <Card style={{ width: '18rem' }} border="secondary" bg="">
+  <Card.Body>
+  <Card.Header bg="info"> <Card.Title>Clothes</Card.Title></Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1470309864661-68328b2cd0a5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Text className="text-center">
+      Find your favourite books here, anything from fiction to non-fiction
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+      <div>
+      <Card style={{ width: '18rem' }} border="secondary" bg="">
+  <Card.Body>
+  <Card.Header bg="info"> <Card.Title>Games</Card.Title></Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1500995617113-cf789362a3e1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Text className="text-center">
+      Find your favourite books here, anything from fiction to non-fiction
     </Card.Text>
     <Card.Link href="#">Card Link</Card.Link>
     <Card.Link href="#">Another Link</Card.Link>
@@ -49,34 +70,43 @@ export default class Carousel extends Component {
 </Card>
       </div>
       <div>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-        />
+      <Card style={{ width: '18rem' }} border="secondary" bg="">
+  <Card.Body>
+  <Card.Header bg="info"> <Card.Title>Music</Card.Title></Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1511379938547-c1f69419868d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Text className="text-center">
+      Find your favourite books here, anything from fiction to non-fiction
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
       </div>
       <div>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-        />
+      <Card style={{ width: '18rem' }} border="secondary" bg="">
+  <Card.Body>
+  <Card.Header bg="info"> <Card.Title>Garden & Home</Card.Title></Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1458245201577-fc8a130b8829?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Text className="text-center">
+      Find your favourite books here, anything from fiction to non-fiction
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
       </div>
       <div>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-        />
-      </div>
-      <div>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-        />
-      </div>
-      <div>
-        <img
-          alt=''
-          src='https://images.unsplash.com/photo-1572715655204-47e297d3b6dd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=800&q=60'
-        />
+      <Card style={{ width: '18rem' }} border="secondary" bg="">
+  <Card.Body>
+  <Card.Header bg="info"> <Card.Title>Toys</Card.Title></Card.Header>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1520627977056-c307aeb9a625?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+    <Card.Text className="text-center">
+      Find your favourite books here, anything from fiction to non-fiction
+    </Card.Text>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
       </div>
     </InfiniteCarousel>
     </div>
