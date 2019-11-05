fb14ed3e65f68785f840ee9e95cfabc286028825
Oliver Antcliff <antcliffoc@gmail.com>
2019-11-05 14:57:55 +0000

    you are here marker

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { InfoWindow, Marker, Map, GoogleApiWrapper } from 'google-maps-react';
 
 const mapStyles = {
   width: '100vw',
@@ -7,7 +7,33 @@ const mapStyles = {
 };
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if(this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
+
   render() {
+    let icon = {
+      url: "http://maps.google.com/mapfiles/ms/icons/yellow-dot.png"
+    }
     return (
       <Map
         google={this.props.google}
@@ -17,11 +43,24 @@ export class MapContainer extends Component {
          lat: 51.51746,
          lng: -0.07329
         }}
-      />
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          icon={icon}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div><h4>Nearly</h4></div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
 
 export default GoogleApiWrapper({
-  apiKey: ''
+  apiKey: 'AIzaSyDfkHZsny6TVUsJjoS-Jo3okXAhEKYMtCM'
 })(MapContainer);
