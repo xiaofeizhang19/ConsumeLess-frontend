0c0cac427593528ea4ccbae9d421e2ede4d70bce
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MBP.lan>
2019-11-05 00:19:29 +0000

    Move files to components folder

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+// import Navigation from './Navigation';
+import Login from './Login';
+// import AuthService from './AuthService';
+// import withAuth from './withAuth';
+// const Auth = new AuthService();
+
+function App() {
+  return (
+    <>
+      <div><h2>Welcome </h2></div>
+      <div><Login /></div>
+    </>  
+  );
+}
+
+export default App;
