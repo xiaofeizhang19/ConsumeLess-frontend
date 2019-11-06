584e4c617ad4e6b01aa425a8c1bb20cea0258ea9
Xiaofei Zhang <xiaofeizhang@Xiaofeis-MacBook-Pro.local>
2019-11-06 11:38:53 +0000

    Add url for user

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -2,5 +2,6 @@ export const URLs = {
     items: "http://consumerless-backend.herokuapp.com/api/item/index",
     newItem: "http://consumerless-backend.herokuapp.com/api/item/new",
     login: "http://consumerless-backend.herokuapp.com/login",
-    register: "http://consumerless-backend.herokuapp.com/api/user/new"
+    register: "http://consumerless-backend.herokuapp.com/api/user/new",
+    user: "http://consumerless-backend.herokuapp.com/api/user/"
 }
