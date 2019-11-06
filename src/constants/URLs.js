d88af8348a2f05204024e8cdf8a7a4a10395fc7b
Oliver Antcliff <antcliffoc@gmail.com>
2019-11-06 15:40:43 +0000

    }

--- src/constants/URLs.js
+++ src/constants/URLs.js
@@ -11,3 +11,4 @@ export const URLs = {
     // login: "http://consumerless-backend.herokuapp.com/login",
     // register: "http://consumerless-backend.herokuapp.com/api/user/new",
     // user: "http://consumerless-backend.herokuapp.com/api/user/"
+}
