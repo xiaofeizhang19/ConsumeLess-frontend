4d06aa45ccc7976c1cf6c72a3271305451a792a5
Neil Campbell <neildcampbell@hotmail.com>
2019-11-06 12:13:13 +0000

    added columns

--- src/components/Home.js
+++ src/components/Home.js
@@ -5,29 +5,96 @@ import Jumbotron from 'react-bootstrap/Jumbotron'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
-import Image from 'react-bootstrap/Image'
+import Image from 'react-bootstrap/Image';
+import ListGroup from 'react-bootstrap/ListGroup';
+import ListGroupItem from 'react-bootstrap/ListGroupItem';
+import Table from 'react-bootstrap/Table';
+import AuthService from './AuthService';
+import getData from '../actions/getData';
+import { URLs } from '../constants/URLs';
+
 
 
 export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      user: {
+        username: '',
+        email: '',
+        created_at: '',
+    } }
+    this.Auth = new AuthService();
+  }
+  async componentDidMount() {
+  
+    const userId = this.Auth.getProfile().user_id;
+    console.log(userId)
+    const user = await getData(URLs.user + `${userId}`);
+    console.log(user)
+    this.setState({ user });
+    console.log(this.state.user)
+  }
+  
     render() {
+      const { user } = this.state;
+      console.log(user)
         return (
-            <div>
-                <Jumbotron variant="primary">
-  <h1>Hello, User!</h1>
-  <p>
-    This is a simple hero unit, a simple jumbotron-style component for calling
-    extra attention to featured content or information.
-  </p>
-  <p>
-  <Container>
-  <Row>
-    
+          <div>
+            <Jumbotron variant="primary">
+              
+                <Container>
+                  <Row>
+                    <Col xs={6} md={4}>
+                      <Card style={{ width: '18rem' }}>
+                        <Card.Body>
+                          <Card.Title>{user.username}</Card.Title>
+                            <Card.Text>
+                              Some quick example text to build on the card title and make up the bulk of
+                              the card's content.
+                            </Card.Text>
+                          </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Cras justo odio</ListGroupItem>
+    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
+    </Col>
     <Col xs={6} md={4}>
-      <Image src="https://images.unsplash.com/photo-1571249104671-f5537fb3e137?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=300&q=40" roundedCircle />
+    <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src="https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" />
+  
+  
+</Card>
+    </Col>
+    <Col xs={6} md={4}>
+    <Card style={{ width: '18rem' }}>
+  <Card.Body>
+    <Card.Title>Card Title</Card.Title>
+    <Card.Text>
+      Some quick example text to build on the card title and make up the bulk of
+      the card's content.
+    </Card.Text>
+  </Card.Body>
+  <ListGroup className="list-group-flush">
+    <ListGroupItem>Cras justo odio</ListGroupItem>
+    <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
+    <ListGroupItem>Vestibulum at eros</ListGroupItem>
+  </ListGroup>
+  <Card.Body>
+    <Card.Link href="#">Card Link</Card.Link>
+    <Card.Link href="#">Another Link</Card.Link>
+  </Card.Body>
+</Card>
     </Col>
   </Row>
 </Container>
-  </p>
+  
 </Jumbotron>
                 <Accordion defaultActiveKey="0">
   <Card>
