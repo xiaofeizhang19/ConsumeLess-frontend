6e395570e49e6d9c4a62b2a8fdea684a62977e5c
Oliver Antcliff <antcliffoc@gmail.com>
2019-11-08 10:38:49 +0000

    tests

--- src/__tests__/item.test.js
+++ src/__tests__/item.test.js
@@ -1,17 +1,17 @@
-// import React from 'react';
-// import { expect } from 'chai';
-// import { configure } from 'enzyme';
-// import { shallow } from 'enzyme';
-// import Items from '../components/Items';
-// import Adapter from 'enzyme-adapter-react-16';
-//
-// configure({ adapter: new Adapter() });
-//
-//
-//   describe('Items component', () => {
-//     it('renders the <Items /> components', () => {
-//       const wrapper = shallow(<Items />);
-//       expect(wrapper.find('div')).to.have.lengthOf(2)
-//     });
-//
-//   });
+import React from 'react';
+import { expect } from 'chai';
+import { configure } from 'enzyme';
+import { shallow } from 'enzyme';
+import Items from '../components/Items';
+import Adapter from 'enzyme-adapter-react-16';
+
+configure({ adapter: new Adapter() });
+
+
+  describe('Items component', () => {
+    it('renders the <Items /> components', () => {
+      const wrapper = shallow(<Items />);
+      expect(wrapper.find('div')).to.have.lengthOf(2)
+    });
+
+  });
--- src/__tests__/newItem.test.js
+++ src/__tests__/newItem.test.js
@@ -1,18 +1,18 @@
-import React from 'react';
-import { expect } from 'chai';
-import { configure } from 'enzyme';
-import { shallow } from 'enzyme';
-import NewItem from '../components/NewItem';
-import Adapter from 'enzyme-adapter-react-16';
-import { useHistory } from 'react';
-
-configure({ adapter: new Adapter() });
-
-describe('NewItem', () => {
-  
-    it('should have a class to wrapper data', () => {    
-        const newItemComponent = shallow(<NewItem />);
-        expect(newItemComponent.find('.container').length).to.equal(1);
-    });
-    
-});
+// import React from 'react';
+// import { expect } from 'chai';
+// import { configure } from 'enzyme';
+// import { shallow } from 'enzyme';
+// import NewItem from '../components/NewItem';
+// import Adapter from 'enzyme-adapter-react-16';
+// import { useHistory } from 'react';
+//
+// configure({ adapter: new Adapter() });
+//
+// describe('NewItem', () => {
+//
+//     it('should have a class to wrapper data', () => {
+//         const newItemComponent = shallow(<NewItem />);
+//         expect(newItemComponent.find('.container').length).to.equal(1);
+//     });
+//
+// });
